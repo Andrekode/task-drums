66fc7d386eddf1108208995205b2647f2456af79
Andrekode <93523910+Andrekode@users.noreply.github.com>
2022-02-10 14:14:23 +0100

    added

---
+++ js/main.js
@@ -0,0 +1,44 @@
+document.addEventListener("keydown", playSound);
+
+function playSound(event) {
+  const sounds = document.querySelectorAll("audio");
+  for (let i = 0; i < sounds.length; i++) {
+    sounds[i].pause();
+    sounds[i].currentTime = 0;
+  }
+  switch (event.key) {
+    case "1":
+      document.getElementById("audio-clap").play();
+      break;
+
+    case "2":
+      document.getElementById("audio-hihat").play();
+      break;
+
+    case "3":
+      document.getElementById("audio-kick").play();
+
+      break;
+
+    case "4":
+      document.getElementById("audio-openhat").play();
+      break;
+
+    case "5":
+      document.getElementById("audio-ride").play();
+      break;
+
+    case "6":
+      document.getElementById("audio-snare").play();
+      break;
+
+    case "7":
+      document.getElementById("audio-tink").play();
+
+      break;
+
+    case "8":
+      document.getElementById("audio-tom").play();
+      break;
+  }
+}
